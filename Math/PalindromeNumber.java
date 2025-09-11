85200847efb6aaecff85540cceb80e594350b65a
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-09-10 22:35:43 -0500

    Java - Palindrome Number

---
+++ Math/PalindromeNumber.java
@@ -0,0 +1,46 @@
+package EASY;
+/*
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Example 1:
+    Input: x = 121
+    Output: true
+    Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+    Input: x = -121
+    Output: false
+    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+    Input: x = 10
+    Output: false
+    Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Constraints:
+
+-231 <= x <= 231 - 1
+*/
+
+class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        
+        if (x < 0 || (x % 10 == 0 && x != 0)){
+            return false;
+        }
+
+        int reversed = 0;
+        while(x > reversed){
+            int lastDigit = x % 10;
+            reversed = reversed * 10 + lastDigit;
+            x /= 10;
+        }
+        return x == reversed ||  x == reversed / 10;
+    }
+
+    public static void main(String[] args) {
+        PalindromeNumber obj = new PalindromeNumber();
+        System.out.println(obj.isPalindrome(121)); // true
+        System.out.println(obj.isPalindrome(-121)); // false
+        System.out.println(obj.isPalindrome(10)); // false
+    }
+}
