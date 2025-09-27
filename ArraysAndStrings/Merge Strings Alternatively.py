4bdb7db211e1ee51ed93758f5e0fb934ed7a90a5
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-09-26 22:49:17 -0500

    Python - Merge Strings Alternatively

---
+++ ArraysAndStrings/Merge Strings Alternatively.py
@@ -0,0 +1,53 @@
+'''
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, 
+starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+Example 1:
+    Input: word1 = "abc", word2 = "pqr"
+    Output: "apbqcr"
+    Explanation: The merged string will be merged as so:
+    word1:  a   b   c
+    word2:    p   q   r
+    merged: a p b q c r
+Example 2:
+    Input: word1 = "ab", word2 = "pqrs"
+    Output: "apbqrs"
+    Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+    word1:  a   b 
+    word2:    p   q   r   s
+    merged: a p b q   r   s
+Example 3:
+    Input: word1 = "abcd", word2 = "pq"
+    Output: "apbqcd"
+    Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+    word1:  a   b   c   d
+    word2:    p   q 
+    merged: a p b q c   d
+'''
+
+def mergeAlternately(word1, word2):
+
+    result = []     # Result will contain the merged words
+    i, j = 0, 0     # initialize pointers for each word
+
+    # Merge words alternatively by using pointers incrementally
+    while i < len(word1) and j < len(word2):
+        result.append(word1[i])
+        result.append(word2[j])
+        i += 1
+        j += 1
+
+    # Append the remaining characters from word1 and word2
+    while i < len(word1):
+        result.append(word1[i:])
+        i += 1
+
+    while j < len(word2):
+        result.append(word2[j:])
+        j += 1
+
+    # Return the merged string  
+    return "".join(result)
