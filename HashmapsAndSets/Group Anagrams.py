71038b68a1bb5959380a55210329c4d1f133fa95
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-11-01 00:00:36 -0500

    Python - Group Anagrams

--- HashmapsAndSets/Group Anagrams.py
+++ HashmapsAndSets/Group Anagrams.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 """
 Given an array of strings strs, group the anagrams together. 
 You can return the answer in any order.
@@ -29,9 +30,24 @@ def group_anagrams(strs):
             anagrams[word].append(strs[i])
     return anagrams.values()
 
+def group_anagrams1(strs):
+    hash_table = defaultdict(list)
+    for word in strs:
+        k = [0] * 26    # key
+        for letter in word:
+            # Have ASCII value subreacted to "a" to put in key k
+            k[ord(letter) - ord("a")] += 1
+        hash_table[tuple(k)].append(word)
+
+    return (hash_table.values())
+
+
 s1 = ["eat","tea","tan","ate","nat","bat"]
 s2 = [""]
 s3 = ["a"]
 print(group_anagrams(s1))
-print(group_anagrams(s))
+print(group_anagrams1(s1))
+print(group_anagrams(s2))
+print(group_anagrams1(s2))
 print(group_anagrams(s3))
+print(group_anagrams1(s3))
